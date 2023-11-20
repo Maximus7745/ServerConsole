ffded1dd80283c545981acb2459af9825067907b
Maksim <maksimtomilov3377@gmail.com>
2023-11-20 21:26:48 +0500

    add files

---
+++ ServerConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using ServerConsole;
+
+Parallel.For(0, 50, i =>
+{
+    if (i % 5 == 0)
+        Server.AddToCount(i);
+    else
+        Console.WriteLine(Server.GetCount());
+});
+
---
+++ ServerConsole/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConsole
+{
+    public static class Server
+    {
+        private static int count = 0;
+        static ReaderWriterLock countLock = new ReaderWriterLock();
+        public static int GetCount()
+        {
+            countLock.AcquireReaderLock(Timeout.InfiniteTimeSpan);
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                countLock.ReleaseReaderLock();
+            }
+        }
+        public static void AddToCount(int value)
+        {
+            countLock.AcquireWriterLock(Timeout.InfiniteTimeSpan);
+            try
+            {
+                count += value;
+            }
+            finally
+            {
+                countLock.ReleaseWriterLock();
+            }
+        }
+
+
+    }
+}
